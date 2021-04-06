83515cf3562ca20b6025721358e3f93714a242b9
HonoR974 <honore.guillaudeau1@gmail.com>
2021-04-06 13:50:44 +0200

    update sendgrid

--- Batch/src/main/java/com/example/Scheduler.java
+++ Batch/src/main/java/com/example/Scheduler.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class Scheduler {
@@ -10,8 +14,11 @@ public class Scheduler {
     @Autowired
     private BatchLauncher batchLauncher;
 
-    @Scheduled(fixedDelay = 8000)
+    //
+    @Scheduled(cron = "*/10 32 13 06 04 ? ")
     public void perform() throws Exception {
         batchLauncher.run();
     }
+
+
 }
